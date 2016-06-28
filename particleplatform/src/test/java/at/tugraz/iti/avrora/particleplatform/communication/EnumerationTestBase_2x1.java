a6c14daeb45b8dfef27703b73941d719f9e3d700
Raoul Rubien <rubienr@gmail.com>
2016-06-28 19:50:13 +0200

    cosmetics

--- particleplatform/src/test/java/at/tugraz/iti/avrora/particleplatform/communication/EnumerationTestBase_2x1.java
+++ particleplatform/src/test/java/at/tugraz/iti/avrora/particleplatform/communication/EnumerationTestBase_2x1.java
@@ -30,9 +30,9 @@ import static junit.framework.TestCase.assertFalse;
 public class EnumerationTestBase_2x1 {
 
     static private final Logger LOGGER = LoggerFactory.getLogger(EnumerationTestBase_2x1.class);
-    static protected short numberOfRows = 2;
-    static protected short numberOfColumns = 1;
-    protected static double simulationSeconds = 1E-3 * 35f;
+    protected static short numberOfRows = 2;
+    protected static short numberOfColumns = 1;
+    protected static double simulationSeconds = 1E-3 * 15f;
     static private Options mainOptions = null;// = new Options();
     static private ByteArrayOutputStream systemOutBuffer = null;// = new ByteArrayOutputStream();
     @Rule
