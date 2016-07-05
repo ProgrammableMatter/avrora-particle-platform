28ebc3effeadaaedf841bf3fecdbcb94f458c862
Raoul Rubien <rubienr@gmail.com>
2016-07-05 11:40:44 +0200

    added new test case

---
+++ particleplatform/src/test/java/at/tugraz/iti/particleplatform/protocol/enumeration/EnumerationTest_1x3.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2016
+ * Raoul Rubien
+ */
+
+package at.tugraz.iti.particleplatform.protocol.enumeration;
+
+import org.junit.BeforeClass;
+
+import java.io.IOException;
+
+/**
+ * Created by Raoul Rubien on 27.05.16.
+ */
+public class EnumerationTest_1x3 extends EnumerationTestBase_2x1 {
+    @BeforeClass
+    public static void startSimulation() throws IllegalAccessException, NoSuchFieldException, IOException {
+        EnumerationTestBase_2x1.numberOfRows = 1;
+        EnumerationTestBase_2x1.numberOfColumns = 3;
+        EnumerationTestBase_2x1.simulationSeconds = 1E-3 * 60f;
+        EnumerationTestBase_2x1.startSimulation();
+    }
+}
