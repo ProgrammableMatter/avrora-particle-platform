2cf5855891c5c6ca86204ea76067b16c620e1f6d
Raoul Rubien <rubienr@gmail.com>
2016-09-08 19:46:30 +0200

    updated configuration

--- src/test/java/at/tugraz/iti/SimulationTestBase_1x1.java
+++ src/test/java/at/tugraz/iti/SimulationTestBase_1x1.java
@@ -57,8 +57,8 @@ public class SimulationTestBase_1x1 {
     protected static String firmware = "particle-simulation/main/ParticleSimulation.elf";
     protected static String communicationUnitFirmware = null;
     private static String userHomeDirectory = System.getProperty("user.home") + "/";
-    private static String firmwaresBaseDirectory = ".CLion2016" + "" +
-            ".1/system/cmake/generated/avr-c14d54a/c14d54a/Debug/";
+    private static String firmwaresBaseDirectory =
+            ".CLion2016.1/system/cmake/generated/src-c0e425f7/c0e425f7/Debug/";
     private static long lastSimulationRealDuration = -1;
     private static long lastInspectionDuration;
 
